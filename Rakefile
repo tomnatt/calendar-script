e07c6fe60ac957b64e7cf53f95afbfd99c9a18f6
tomnatt <github@tomnatt.com>
2026-02-12 14:20:41 +0000

    Partial implementation of Work Week counter

--- Rakefile
+++ Rakefile
@@ -1,9 +1,22 @@
+require './lib/calendar'
+require './lib/gym_slots'
+
 task :default do
   Rake::Task['list_entries'].invoke
 end
 
-# List calendar entries that match name pattern
 desc 'List calendar entries that match name pattern'
 task :list_entries do
-  puts 'foo'
+  cal = GymSlots.new
+end
+
+desc 'List work entries and total hours worked for a given week'
+task :work, [:monday] do |_t, args|
+  monday = args[:monday] or abort 'Provide Monday date YYYY-MM-DD'
+
+  cal = Calendar.new
+  # worked = cal.hours_worked(monday)
+  cal.hours_worked(monday)
+
+  # puts worked.inspect
 end
---
+++ lib/calendar.rb
@@ -0,0 +1,47 @@
+require 'date'
+require 'google/apis/calendar_v3'
+require 'googleauth'
+
+class Calendar
+  def initialize
+    json_key_file = ENV.fetch('GOOGLE_CALENDAR_SERVICE_ACCOUNT_KEY_FILE')
+    abort 'Missing key file' if json_key_file.nil?
+
+    @service = Google::Apis::CalendarV3::CalendarService.new
+    creds = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: File.open(json_key_file),
+      scope:       Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
+    )
+
+    creds.fetch_access_token!
+    @service.authorization = creds
+  end
+
+  def hours_worked(monday_input)
+    monday = Date.parse(monday_input)
+    sunday = monday + 7
+
+    calendar_id = ENV.fetch('GOOGLE_CALENDAR_ID')
+    title = 'Work'
+
+    events = list_events(calendar_id, title, monday, sunday)
+    create_worked_output(events)
+  end
+
+  def list_events(calendar_id, title, period_start, period_end)
+    @service.list_events(
+      calendar_id,
+      q:             title,
+      single_events: true,
+      order_by:      'startTime',
+      time_min:      period_start.to_time.utc.iso8601,
+      time_max:      period_end.to_time.utc.iso8601
+    )
+  end
+
+  def create_worked_output(events)
+    events.items.each do |event|
+      puts event.summary
+    end
+  end
+end
